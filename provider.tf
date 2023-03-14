2982dfe4275dbe7405724c8a8779a6d22e9b8bd7
Ryan Higdn <ryan.higdon@gmail.com>
2023-03-13 21:25:10 -0400

    Update provider.tf
    fixes oci provider to remove warning

--- provider.tf
+++ provider.tf
@@ -1,3 +1,11 @@
+terraform {
+  required_providers {
+    oci = {
+      source = "oracle/oci"
+      version = "4.111.0"
+    }
+  }
+}
 provider "oci" {
   tenancy_ocid = var.tenancy_ocid
   user_ocid = var.user_ocid
